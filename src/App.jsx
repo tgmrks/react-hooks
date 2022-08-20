7f3322e05d92dd4606eb995fc58294c7b20a30fc
tgmrks <tgmrks@gmail.com>
2022-08-20 11:33:58 -0300

    props example

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import {Classprops} from './Classprops'
+import Functionprops from './Functionprops';
+
+function App() {
+  return (
+    <div>
+      <Classprops name="Ismael"/>
+      <Functionprops name="Thiago"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Functionprops.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Functionprops(props) {
+    return (
+        <div>Hi there! {props.name}</div>
+    )
+}
+export default Functionprops;
