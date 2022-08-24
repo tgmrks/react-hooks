24d8907f0a0bcff273d96fa2f2b401e188d17e67
tgmrks <tgmrks@gmail.com>
2022-08-24 18:00:47 -0300

    data fetching example

--- src/Effect.jsx
+++ src/Effect.jsx
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Effect() {
     const [count, setCount] = useState(0)
     const [timer, setTimer] = useState(0)
+    const [data, setData] = useState("")
 
     useEffect(() => {
-        setTimeout(() => { setTimer(timer + 1) }, 1000);
-        console.log("Effect called!");
-    }, [timer]);
+        console.log("Effect called for TIMER !");
+        setTimeout(() => { setTimer(timer + 1) }, 5000);
+        axios.get("https://jsonplaceholder.typicode.com/comments")
+            .then((response) => {
+                console.log("Effect called for COUNT !");
+                setData(response.data[count].email);
+            });
+    }, [timer, count]);
 
     return (
         <div>
+            <h3>{data}</h3>
+            <hr />
             <h3>Click count: {count}</h3>
             <button onClick={() => { setCount(count + 1) }}>+</button>
             <hr />
