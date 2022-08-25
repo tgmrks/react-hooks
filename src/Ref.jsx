acda1dc8354ac0ea9167e72fac370da98c684f94
tgmrks <tgmrks@gmail.com>
2022-08-25 10:39:59 -0300

    previous value example

--- src/Ref.jsx
+++ src/Ref.jsx
@@ -1,12 +1,17 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 function Ref() {
     const [name, setName] = useState("")
     const inputRef = useRef()
+    const prevName = useRef('')
 
     function Focus() {
         inputRef.current.focus()
     }
+
+    useEffect(() => {
+        prevName.current = name
+    }, [name])
 
     return (
         <div>
@@ -14,7 +19,9 @@ function Ref() {
                 ref={inputRef} type="text"
                 onChange={e => setName(e.target.value)}
             />
-            <p>My name is {name}</p>
+            <p>
+                My name is [{name}] and it used to be [{prevName.current}]
+            </p>
             <button onClick={Focus}>Focus</button>
         </div>
     );
